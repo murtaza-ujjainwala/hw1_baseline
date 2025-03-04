c5b7d989d11bc1bac825974a85a7c989d0db3f05
https://github.com/murtmurt <tazujjainwala@gmail.com>
2025-03-04 13:41:42 -0500

    updated API documentation for ExpenseTrackerApp class

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-# hw1
+# Expense Tracker App
 
-This folder contains baseline code for homework1-manual review.
+## Overview
+
+The Expense Tracker App is a simple Java application that allows users to add daily transactions.
+
+## Supported Features:
+
+- **Add Transactions:** Users can input the amount of money corresponding to the transaction, as well as the category it belongs to.
+- **View Transactions:** Users will be able to view their transaction history from the table.
+- **Total Sum:** Users will be able to view the total sum of their transaction amounts at the bottom of the table.
+- **Open-box Extensibility:** The app can be easily extended or modified to add features.
+
+## API for the `ExpenseTrackerApp` class:
+
--- expense_tracker/src/ExpenseTrackerView.java
+++ expense_tracker/src/ExpenseTrackerView.java
@@ -1,12 +1,13 @@
-
-
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
-
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.List; 
 
+/*
+ * The ExpenseTrackerView class creates the User Interface for the Expense Tracker application.
+ * It provides the methods for the input fields (used for entering transactions), the table displaying transaction history, and the button for adding transactions.
+ */
 public class ExpenseTrackerView extends JFrame {
 
   private JTable transactionsTable;
@@ -16,12 +17,18 @@ public class ExpenseTrackerView extends JFrame {
   private DefaultTableModel model;
   private List<Transaction> transactions = new ArrayList<>();
 
-  
-
+  /*
+   * Gets the transaction history table.
+   * @return The JTable that displays transaction history.
+   */
   public JTable getTransactionsTable() {
     return transactionsTable;
   }
 
+  /*
+   * Gets the data that is in the amount field.
+   * @return The amount that the user entered (0 if no amount entered).
+   */
   public double getAmountField() {
     if(amountField.getText().isEmpty()) {
       return 0;
@@ -31,25 +38,50 @@ public class ExpenseTrackerView extends JFrame {
     }
   }
 
+  /*
+   * Sets the amount field.
+   * @param amountField A JTextField to enter the amount.
+   */
   public void setAmountField(JTextField amountField) {
     this.amountField = amountField;
   }
 
+  /*
+   * Gets the data that is in the category field.
+   * @return The category that the user entered.
+   */
   public String getCategoryField() {
     return categoryField.getText();
   }
 
+  /*
+   * Sets the category field.
+   * @param categoryField A JTextField to enter the category.
+   */
   public void setCategoryField(JTextField categoryField) {
     this.categoryField = categoryField;
   }
 
+  /*
+   * Gets the "Add Transaction" button.
+   * @return The JButton that allows a user to add transactions to the transaction history table.
+   */
   public JButton getAddTransactionBtn() {
     return addTransactionBtn;
   }
+
+  /*
+   * Gets the table model used for the transaction history.
+   * @return The DefaultTableModel that is used for storing the transaction history data.
+   */
   public DefaultTableModel getTableModel() {
     return model;
   }
 
+  /*
+   * Creates the user interface.
+   * @param model The table model used for storing and displaying the user's transaction history.
+   */
   public ExpenseTrackerView(DefaultTableModel model) {
     setTitle("Expense Tracker"); // Set title
     setSize(600, 400); // Make GUI larger
@@ -88,6 +120,10 @@ public class ExpenseTrackerView extends JFrame {
   
   }
 
+  /*
+   * Updates the transaction history table with a new row if a new transaction has been added by the user. Also updates the total sum of transactions.
+   * @param transactions The list of transactions that should be shown in the table.
+   */
   public void refreshTable(List<Transaction> transactions) {
       // model.setRowCount(0);
       model.setRowCount(0);
@@ -110,6 +146,9 @@ public class ExpenseTrackerView extends JFrame {
   
     }  
 
+  /*
+   * Gets the updated list of transactions and calls the refreshTable method.
+   */
   public void refresh() {
 
     // Get transactions from model
@@ -120,17 +159,23 @@ public class ExpenseTrackerView extends JFrame {
   
   }
 
+  /*
+   * Gets the list of transactions that the user has added.
+   * @return The list of transactions.
+   */
   public List<Transaction> getTransactions() {
     return transactions;
   }
   
+  /*
+   * Adds a transaction to the list of transactions and refreshes the table to show the new transaction in a new row.
+   * @param t The new transaction that will be added.
+   */
   public void addTransaction(Transaction t) {
     transactions.add(t);
     getTableModel().addRow(new Object[]{t.getAmount(), t.getCategory(), t.getTimestamp()});
     refresh();
   }
-  
-
 
   // Other view methods
 }
